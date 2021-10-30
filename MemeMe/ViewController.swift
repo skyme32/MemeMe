bc08d381118447bcb3e660a593460752627eccab
Marcos <mmejiasg@zoho.com>
2021-10-30 17:38:15 +0200

    Innit commit
    Init commit with picker controller to library.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: IBOutlet properties
+    @IBOutlet weak var imagePicker: UIImageView!
+    
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func pickAnImage(_ sender: Any) {
+        showImagePickerController()
+    }
+    
+}
 
+// MARK: Control Picker controller
+extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    func showImagePickerController() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imagePicker.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
