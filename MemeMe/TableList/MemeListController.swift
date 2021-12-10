0e5deb4719793cd14f764970c6e1cb90af2e3265
Marcos <mmejiasg@zoho.com>
2021-12-10 17:58:59 +0100

    Add ViewControllers
    Type: Development
    
    Add he navigation controller with List and Collection.
    
    Change storyboard.

---
+++ MemeMe/TableList/MemeListController.swift
@@ -0,0 +1,46 @@
+//
+//  MemeListController.swift
+//  MemeMe
+//
+//  Created by skyme32 on 10/12/21.
+//
+
+import UIKit
+
+class MemeListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MemeListController: UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: Table View Data Source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+}
